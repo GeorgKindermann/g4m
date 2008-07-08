1ea57c1d94b82c8097e3887378cf79e151b3e490
georg.kindermann <georg.kindermann@56aa75cb-0951-0410-b038-050d5bff27f0>
2008-07-08 17:12:55 +0000

    Added multidimmensional linear interpolation.

--- trunk/increment/misc.h
+++ trunk/increment/misc.h
@@ -2,6 +2,9 @@
 #define G4M_MISC_H
 
 #include <map>
+#include <vector>
+
+#include <iostream>
 
 namespace g4m {
 
@@ -70,6 +73,89 @@ namespace g4m {
     VAL ipol<IDX, VAL>::operator[](const IDX i) {
     return(g(i));
   }
+
+
+  //Multidimensional interpolation
+  template <class IDX, class VAL>
+    class ipol<std::vector<IDX>, VAL> {
+  public:
+    void insert(std::vector<IDX>, VAL);
+    VAL g(const std::vector<IDX>);   //returns the value
+    void clear();
+    VAL operator[](const std::vector<IDX> i);
+   private:
+    std::map<std::vector<IDX>, VAL> aMap; //Data Map
+  };
+
+  template <class IDX, class VAL>
+  void ipol<std::vector<IDX>, VAL>::clear() {
+    aMap.clear();
+  }
+
+  template <class IDX, class VAL>
+  void ipol<std::vector<IDX>, VAL>::insert(std::vector<IDX> i, VAL v) {
+    aMap.insert(make_pair(i, v));
+  }
+
+  template <class IDX, class VAL>
+    VAL ipol<std::vector<IDX>, VAL>::g(const std::vector<IDX> i) {
+    typename std::map<std::vector<IDX>, VAL>::iterator iter;
+    unsigned int regions = 1;
+    for(unsigned int j=0; j<i.size(); ++j) {regions *= 2;}
+    std::vector<VAL>* y = new std::vector<VAL>[regions];
+    double* dist = new double[regions]; //shortest distance in region
+    for(unsigned int j=0; j < regions; ++j) {dist[j] = -1.;}
+    for(iter = aMap.begin(); iter != aMap.end(); ++iter) {
+      double d=0.;
+      int pos=0;
+      int mul=1;
+      for(unsigned int j=0; j < iter->first.size() && j < i.size(); ++j) {
+	double tmp = iter->first[j] - i[j];
+	if(tmp > 0.) {pos += mul;}
+	d += tmp * tmp;
+	mul *= 2;
+      }
+      if(d <= dist[pos] || dist[pos]<0.) {
+	if(d < dist[pos] || dist[pos]<0.) {y[pos].clear(); dist[pos] = d;}
+	y[pos].push_back(iter->second); 
+      }
+    }
+     double ip = 0.;
+    double distsum = 0.;
+    int n = 0;
+    for(unsigned int j=0; j < regions; ++j) {
+      if(dist[j] > 0. && n==0) {
+	for(unsigned int k=0; k<y[j].size(); ++k) {
+	  ip += y[j][k] * 1./dist[j];
+	  distsum += 1./dist[j];
+	}
+      }
+      else if(dist[j] == 0.) {
+	if(n==0) {ip = 0.;}
+	for(unsigned int k=0; k<y[j].size(); ++k) {
+	  ip += y[j][k];
+	  ++n;
+	}
+      }
+    }
+    if(n > 0) {ip /= n;}
+    else if(distsum > 0.) {ip /= distsum;}
+    else {ip = 0.;}
+    VAL ret = ip;
+    delete[] y;
+    delete[] dist;
+    return(ret);
+  }
+
+  template <class IDX, class VAL>
+    VAL ipol<std::vector<IDX>, VAL>::operator[](const std::vector<IDX> i) {
+    return(g(i));
+  }
+
+
+  //Fast Interploation where the steps of the index are 1
+  //and the size of the data array is known in advance
+  //and complete filled with values
 
 }
 
--- trunk/increment/testMisc.cc
+++ trunk/increment/testMisc.cc
@@ -6,10 +6,33 @@ using namespace std;
 
 
 int main() {
+  //Test one dimensional interpolation
   g4m::ipol<double, double> d;
   d.insert(3, 0.);
   d.insert(5, 10.);
   cout << d.g(4.5) << endl;
-  cout << d[4.5] << endl;
+  cout << d[5.5] << endl;
+
+  //Test multi dimensional interpolation
+  g4m::ipol<vector<double>, double> vd;
+  vector<double> idx;
+  idx.push_back(2.);
+  idx.push_back(5.);
+  idx.push_back(3.);
+  
+  vd.insert(idx, 7.);
+
+  idx[0] = 7; idx[1] = -3; idx[2] = 5;
+  vd.insert(idx, 2.2);
+
+  cout << vd.g(idx) << endl;
+  idx[0] = 5; idx[1] = -3; idx[2] = 5;
+  cout << vd[idx] << endl;
+  idx[0] = 2; idx[1] = 5; idx[2] = 3;
+  cout << vd.g(idx) << endl;
+
+  vd.clear();
+  idx.clear();
+
   return(0);
 }

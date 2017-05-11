b9b5f396b8f2a94617216c649988dae3fee8d8f8
Georg Kindermann <georg@pc97192.iiasa.ac.at>
2017-05-11 09:24:11 +0200

    Removed unneeded files

--- harvest/plot.r
+++
@@ -1,8 +0,0 @@
-x <- read.table("/tmp/out.txt", header = T)
-
-pdf("/tmp/plots.pdf")
-for(i in 2:NCOL(x)) {
-  plot(x$vs, x[,i], main=names(x)[i], type="l", xlab="Stemvolume [m3]", ylab="Time", lwd=3)
-}
-dev.off()
-

c465b955c11ff5c60e631810fd139eb76e3fafae
Georg Kindermann <kinder@iiasa.ac.at>
2018-09-13 17:18:03 +0200

    Removed bugs in aggregation

--- application/18maiRotTime4Fulvio/3aggregate.pl
+++ application/18maiRotTime4Fulvio/3aggregate.pl
@@ -10,8 +10,8 @@ while($line = <STDIN>) {
   if(substr($line, 0, 1) ne '#') {
     @atom = split(/ /,$line);
     $area{$atom[2]} += $atom[3];
-    for($i=3; $i<@head; ++$i) {
-      $sum[$i]{$atom[2]} += $atom[$i];
+    for($i=4; $i<@head; ++$i) {
+      $sum[$i]{$atom[2]} += $atom[$i] * $atom[3];
     }
   }
 }
@@ -21,7 +21,7 @@ print "\n";
 while(my($k, $v) = each %area) {
   print("$k");
   printf " %.0f", $v;
-  for($i=3; $i<@head; ++$i) {
+  for($i=4; $i<@head; ++$i) {
     printf " %.2f ", $sum[$i]{$k}/$v;
   }
   print "\n";
